569d6119e26cb4d1e671f89a92c99ef60fd1a8f9
rhexo <mmusolov@gmail.com>
2015-10-24 00:33:11 +0400

    sync

--- STL/associative_containers_1/main.cpp
+++ STL/associative_containers_1/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <cstdlib>
-
+#include <set>
+#include <map>
 
 using namespace std;
 
 int main ()
 {
 
+  /** SETs */
+  multiset<string> cities {
+    "Braunschwieg", "Hanover", "Frankfurt",
+      "New York", "Chicago", "Toronto", "Paris", "Frankfurt"
+      };
+
+  multimap<int,string> coll;
+  coll.insert(pair<int,string>(100,"some info"));
 
   exit(EXIT_SUCCESS);
 
