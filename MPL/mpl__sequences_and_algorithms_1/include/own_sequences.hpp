98c4d2bdbe82559269562e0ff1c03d97f2a8ba15
rhexo <mmusolov@gmail.com>
2015-12-14 17:58:24 +0400

    add mpl own seuence example

--- MPL/mpl__sequences_and_algorithms_1/include/own_sequences.hpp
+++ MPL/mpl__sequences_and_algorithms_1/include/own_sequences.hpp
@@ -2,52 +2,115 @@
 #include <boost/mpl/empty.hpp>
 #include <boost/mpl/identity.hpp>
 #include <boost/mpl/pop_front.hpp>
+#include <boost/mpl/iterator_tags.hpp>
+#include <boost/mpl/at.hpp>
 
-namespace seq {
 
-  using namespace std;
-  using namespace boost::mpl::placeholders;
+namespace boost {
+  namespace mpl {
 
-  namespace mpl = boost::mpl;
+    /**
+     * Writing a tiny sequence
+     */
 
-  /**
-   * Writing a tiny sequence
-   */
+    struct none {}; // tag type to denote no element
 
-  struct none {}; // tag type to denote no element
+    // add ::tag to tiny
 
-  template <class T0 = none, class T1 = none, class T2 = none>
-  struct tiny {
-    typedef tiny type;
-    typedef T0 t0;
-    typedef T1 t1;
-    typedef T2 t2;
-  };
+    struct tiny_tag {};
 
-  // pop front element of S, unless it`s empty
-  typedef mpl::eval_if<
-    mpl::empty<S>,
-    mpl::identity<S>,
-    mpl::pop_front<S>
-    >::type r1;
+    template <class T0 = none, class T1 = none, class T2 = none>
+    struct tiny {
+      typedef tiny_tag tag;
+      typedef tiny type;
+      typedef T0 t0;
+      typedef T1 t1;
+      typedef T2 t2;
+    };
 
-  // likewise
-  typedef mpl::eval_if<
-    mpl::empty<S>,
-    S,  // when invoked, S return S
-    mpl::pop_front<S>
-    >::type r2;
 
-  /**
-   * The other three nested typedefs, t0, t1 and t2, makeit easy for any metafunction to access a tiny sequences elements
-   */
+    /**
+     * The other three nested typedefs t0, t1 and t2, makeit easy for any metafunction to access a tiny sequences elements
+     */
 
-  // tiny elements class wrapper
-  template <class Tiny>
-  struct manipulate_tiny
-  {
-    // what`s T0?
-    typedef typename Tiny::t0 t0;
-  };
+    // tiny elements class wrapper
+    template <class Tiny>
+    struct manipulate_tiny
+    {
+      // what`s T0?
+      typedef typename Tiny::t0 t0;
+    };
 
-};
+    /**
+     * Implementing at for tiny
+     */
+    template <class Tiny, int N> struct tiny_at;
+
+    // partially specialized accessors for each index
+    template <class Tiny>
+    struct tiny_at<Tiny,0>
+    {
+      typedef typename Tiny::t0 type;
+    };
+
+    template <class Tiny>
+    struct tiny_at<Tiny, 1>
+    {
+      typedef typename Tiny::t1 type;
+    };
+
+    template <class Tiny>
+    struct tiny_at<Tiny,2>
+    {
+      typedef typename Tiny::t2 type;
+    };
+
+
+    // own iterator metaclass
+    template <class Tiny, class Pos>
+    struct tiny_iterator
+    {
+      typedef random_access_iterator_tag category;
+    };
+
+    // own next/prior handlers
+
+    // forward iterator requirement
+    template <class Tiny, class Pos>
+    struct next<tiny_iterator<Tiny, Pos> >
+    {
+      typedef tiny_iterator<
+        Tiny,
+        typename next<Pos>::type
+        > type;
+    };
+
+    // bidirectional operator requirement
+    template <class Tiny, class Pos>
+    struct prior<tiny_iterator<Tiny,Pos> >
+    {
+      typedef tiny_iterator<
+        Tiny,
+        typename prior<Pos>::type
+        > type;
+    };
+
+
+    // template <class T0, class T1, class T2, class Pos >
+    // struct at<tiny<T0,T1,T2>,Pos >
+    //   : tiny_at<tiny<T0,T1,T2>,Pos::value >
+    // {};
+
+    // write an explicit(full) specialization of mpl::at_impl
+    template <>
+    struct at_impl<tiny_tag>
+    {
+      template <class Tiny, class N >
+      struct apply
+        :tiny_at<Tiny, N::value >
+      {};
+    };
+
+
+  }
+}
--- MPL/mpl__sequences_and_algorithms_1/main.cpp
+++ MPL/mpl__sequences_and_algorithms_1/main.cpp
@@ -8,6 +8,7 @@
 #include "include/iterators.hpp"
 #include "include/classes.hpp"
 #include "include/integral_swrappers.hpp"
+#include "include/own_sequences.hpp"
 
 using namespace std;
 namespace mpl=boost::mpl;
@@ -34,6 +35,30 @@ int main(){
   // check next
   BOOST_STATIC_ASSERT((is_same<seq::y,float>::value));
   cout << "seq::y and float compare: " << is_same<seq::y,float>::value << endl;
+
+
+  /**
+   * Own sequences section
+   */
+
+  typedef mpl::tiny<mpl::int_<10>,mpl::int_<20>,mpl::int_<30> > S;
+
+  // pop front element of S, unless it`s empty
+  typedef mpl::eval_if<
+    mpl::empty<S>,
+    mpl::identity<S>,
+    mpl::pop_front<S>
+    >::type r1;
+
+  // likewise
+  typedef mpl::eval_if<
+    mpl::empty<S>,
+    S,  // when invoked, S return S
+    mpl::pop_front<S>
+    >::type r2;
+
+
+  cout << "Tiny.at<2> = " << mpl::at<S,mpl::int_<2> >::type::value << endl;
 
   exit(EXIT_SUCCESS);
 };
