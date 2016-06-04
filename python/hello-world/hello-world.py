a9bb20e8c77279b8c1c0c02fdbc5398b0d84e363
rhexo <mmusolov@gmail.com>
2016-06-04 10:12:22 +0300

    python app added

---
+++ python/hello-world/hello-world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# hello-world.py
+print "Hello world from python!"
+
+a = [66, 22, 33]
+
+a.append(100)
+
+for w in a:
+    print w
+    if w == 66:
+        print 66 * 66
+    if w == 22:
+        print 22 * 22
+    if w == 33:
+        print 33 * 33
+
